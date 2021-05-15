d3c1ca0cc78e43948a52d967dab24bbfb082fa8a
IldarRyabkov <ildar.239@mail.ru>
2021-05-15 11:08:08 +0300

    Improved physics of bubble movement.

--- src/bubble.py
+++ src/bubble.py
@@ -21,9 +21,12 @@ class Bubble:
         self.radius = BUBBLES[bubble_type]["radius"]
         self.health = BUBBLES[bubble_type]["health"]
         self.vel = uniform(0.7, 1.7) * BUBBLE_MAX_VEL
-        self.acc = BUBBLE_ACC
+        self.acc = -BUBBLE_ACC
+        self.gravity_vel = 0
+        self.max_gravity_vel = BUBBLE_MAX_VEL
+        self.gravity_acc = BUBBLE_ACC
         self.angle = angle
-        self.gravitation_radius = gravitation_radius
+        self.gravity_radius = gravitation_radius
         self.body = Body(BUBBLES[bubble_type]["body"])
         self.base_halo = None
         self.halo = None
@@ -36,43 +39,16 @@ class Bubble:
         return not circle_collidepoint(SCR_W2, SCR_H2, ROOM_RADIUS, self.x, self.y)
 
     def in_gravity_zone(self, player_x, player_y):
-        return hypot(self.x - player_x, self.y - player_y) <= self.gravitation_radius
+        return hypot(self.x - player_x, self.y - player_y) <= self.gravity_radius
 
     def is_on_screen(self, dx, dy):
         return (-self.radius <= self.x - dx <= SCR_W + self.radius and
                 -self.radius <= self.y - dy <= SCR_H + self.radius)
 
     def maximize_gravity(self):
-        self.vel = 2 * BUBBLE_MAX_VEL
-        self.gravitation_radius = 2 * ROOM_RADIUS
-
-    def go_to_player(self, x, y, dt):
-        self.acc = BUBBLE_ACC
-        self.angle = calculate_angle(self.x, self.y, x, y)
-        dr = self.vel * dt + self.acc * dt*dt / 2
-        dist = hypot(self.x-x, self.y-y)
-        if dr > dist:
-            self.x = x
-            self.y = y
-        else:
-            self.x += dr * cos(self.angle)
-            self.y -= dr * sin(self.angle)
-        self.vel += self.acc * dt
-        if self.vel >= BUBBLE_MAX_VEL:
-            self.vel = BUBBLE_MAX_VEL
-            self.acc = 0
-
-    def slow_down(self, dt):
-        self.acc = -BUBBLE_ACC
-        dr = self.vel * dt + self.acc * dt*dt / 2
-        self.x += dr * cos(self.angle)
-        self.y -= dr * sin(self.angle)
-        dv = self.acc * dt
-        if self.vel * (self.vel + dv) < 0:
-            self.vel = 0
-            self.acc = 0
-        else:
-            self.vel += dv
+        self.max_gravity_vel = 3 * BUBBLE_MAX_VEL
+        self.gravity_acc = 10 * BUBBLE_ACC
+        self.gravity_radius = 2 * ROOM_RADIUS
 
     def move(self, dx, dy):
         self.x += dx
@@ -88,13 +64,42 @@ class Bubble:
         if self.halo is not None:
             self.update_halo()
 
-    def update(self, x, y, dt):
-        if self.vel or self.is_on_screen(x - SCR_W2, y - SCR_H2):
-            if self.in_gravity_zone(x, y):
-                self.go_to_player(x, y, dt)
-            elif self.vel:
-                self.slow_down(dt)
-            self.update_body(dt)
+    def update(self, player_x, player_y, dt):
+        # update bubble accelerations first
+        if self.vel == 0:
+            self.acc = 0
+        if self.in_gravity_zone(player_x, player_y):
+            self.gravity_acc = BUBBLE_ACC
+        elif self.gravity_vel != 0:
+            self.gravity_acc = -BUBBLE_ACC
+        else:
+            self.gravity_acc = 0
+
+        # Then calculate new coordinates using the formula for uniformly accelerated rectilinear motion
+        gravity_dr = self.gravity_vel * dt + self.gravity_acc * dt*dt/2
+        if gravity_dr > hypot(self.x - player_x, self.y - player_y):
+            # if the displacement of the bubble to the player is greater than the
+            # distance to the player, then the bubble has passed through the player
+            self.x = player_x
+            self.y = player_y
+        else:
+            # Otherwise, a displacement is added to the coordinates as a result of the sum
+            # of two movements: the movement of the bubble due to the initial impulse,
+            # and movement due to the attraction to the player.
+            dr = self.vel * dt + self.acc * dt * dt / 2
+            gravity_angle = calculate_angle(self.x, self.y, player_x, player_y)
+            self.x += dr * cos(self.angle) + gravity_dr * cos(gravity_angle)
+            self.y -= dr * sin(self.angle) + gravity_dr * sin(gravity_angle)
+
+        # After that we update velocities
+        self.vel = max(0, self.vel + self.acc * dt)
+        if self.gravity_acc > 0:
+            self.gravity_vel = min(self.max_gravity_vel, self.gravity_vel + self.gravity_acc * dt)
+        else:
+            self.gravity_vel = max(0, self.gravity_vel + self.gravity_acc * dt)
+
+        # Finally, body of the bubble is updated according to the new coordinates
+        self.update_body(dt)
 
     def draw(self, surface, dx=0, dy=0):
         if self.is_on_screen(dx, dy):
